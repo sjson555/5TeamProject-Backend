246ea41e5b4b487c8bac8cad034fee0ea9e34a66
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-03-30 13:26:23 +0900

    #0 프로젝트 생성

---
+++ 5Team_miniPj_Backend/src/test/java/com/Jellabo/Team_miniPj_Backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jellabo.Team_miniPj_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
